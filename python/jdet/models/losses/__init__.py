eb589e100d02e73b60a7f6fec711012eab3d32b8
yangxue <yangxue0827@126.com>
2022-11-17 15:19:49 +0800

    first commit

---
+++ python/jdet/models/losses/__init__.py
@@ -0,0 +1,7 @@
+from .smooth_l1_loss import SmoothL1Loss
+from .focal_loss import FocalLoss
+from .cross_entropy_loss import CrossEntropyLoss
+from .l1_loss import L1Loss
+from .poly_iou_loss import PolyIoULoss
+from .iou_loss import IoULoss
+from .h2rbox_loss import H2RBoxLoss
---
+++ python/jdet/models/roi_heads/__init__.py
@@ -0,0 +1,14 @@
+from .rbbox_head import *
+from .convfc_rbbox_head import *
+from . import retina_head
+from . import s2anet_head
+from . import rpn_head
+from . import oriented_rpn_head
+from . import oriented_head
+from . import gliding_rpn_head
+from . import gliding_head
+from . import ssd_head
+from . import fasterrcnn_head
+from . import fcos_head
+from . import h2rbox_head
+__all__ = []
